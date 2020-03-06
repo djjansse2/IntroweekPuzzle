31368a5b7a69022a272a37b9bde9a9e68aa181c7
djjansse2 <43000159djjansse2@users.noreply.github.com>
2020-03-06 17:12:30 +0100

    Update - Added command arguments

---
+++ SoftwarePuzzle/ArduinoUNOInterpreter/include/HardwareInterface.h
@@ -0,0 +1,16 @@
+#ifndef HardwareInterface
+#define HardwareInterface
+
+/*
+ * Variables
+ */
+
+struct pin_driver_t led;
+
+/*
+ * Functions
+ */
+
+void hardwareSetup();
+
+#endif
--- SoftwarePuzzle/ArduinoUNOInterpreter/src/main.c
+++ SoftwarePuzzle/ArduinoUNOInterpreter/src/main.c
@@ -1,50 +1,23 @@
 #include "simba.h"
 
-#define LED_ON  'a'
-#define LED_OFF 'b'
-#define WAIT    'c'
+#include "Interpreter.h"
+#include "HardwareInterface.h"
 
-struct pin_driver_t led;
-
-char assembly[10] = {'a', 'c', 'c', 'b', 'c', 'c', 'a', 'c', 'b', 'c'};
+int assembly[8] = {1, 1, 2, 100, 1, 0, 2, 2000};
 
 void performAction(char aAction);
 
 void setup()
 {
-    pin_init(&led, &pin_d2_dev, PIN_OUTPUT);
-    pin_write(&led, 1);
+    hardwareSetup();
+    setAssembly(assembly, 8);
 }
 
 void loop()
 {
-    // thrd_sleep_ms(500);
+    runAssembly();
 
-    // pin_toggle(&led);
-
-    for (int i = 0; i < 10; i++)
-    {
-        performAction(assembly[i]);
-    }
-}
-
-void performAction(char aAction)
-{
-    switch(aAction)
-    {
-        case LED_ON:
-            pin_write(&led, 1);
-            break;
-        case LED_OFF:
-            pin_write(&led, 0);
-            break;
-        case WAIT:
-            thrd_sleep_ms(500);
-            break;
-
-        default:
-            break;
-    }
+    // std_printf(OSTR("Hello World\r\n"));
 }
 
 int main(void)
