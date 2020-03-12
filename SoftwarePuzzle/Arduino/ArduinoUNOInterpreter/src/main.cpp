697c2c9d9208948ffde7663bc13e04ad602439ba
djjansse2 <43000159djjansse2@users.noreply.github.com>
2020-03-12 11:51:46 +0100

    Update - Installed FreeRTOS through Platformio libraries

--- SoftwarePuzzle/Arduino/ArduinoUNOInterpreter/src/main.cpp
+++ SoftwarePuzzle/Arduino/ArduinoUNOInterpreter/src/main.cpp
@@ -1,14 +1,28 @@
 #include <Arduino.h>
 
+#include "Arduino_FreeRTOS.h"
+#include "task.h"
+
 #include "Interpreter.h"
 
 int assembly[8] = {1, 1, 2, 100, 1, 0, 2, 2000};
 Interpreter* myInterpreter = new Interpreter(assembly, 8);
 
+void taskIdle(void *pvParameters);
+
 void setup()
 {
 	pinMode(2, OUTPUT);
 	Serial.begin(9600);
+
+	xTaskCreate (
+		taskIdle,
+		"IdleTask",
+		128,
+		NULL,
+		0,
+		NULL
+	);
 }
 
 void loop()
@@ -16,7 +30,7 @@ void loop()
 	// myInterpreter->runAssembly();
 }
 
-void taskIdle()
+void taskIdle(void *pvParameters)
 {
 	
 }
